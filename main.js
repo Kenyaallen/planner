224681470197c511f8db1db02ddc087916a9bce0
kenya <kenyaallen201@gmail.com>
2020-06-27 20:26:27 -0400

    8:26

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// This returns the first element within the document that matches the specified slector, or group of selectors
+var todaysDate = document.querySelector("#todaysDate")
+
+var todo = document.
+
+$( document ).ready( function() {
+});
+    
+    moment().format('MMMM Do YYYY, h:mm:ss a'); 
+    moment().format('dddd');                   
+    console.log(format);
+}
